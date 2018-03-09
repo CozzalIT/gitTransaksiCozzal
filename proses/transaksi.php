72dae4331fc43d3978b21a640306d4d5638d8eac
CozzalIT <farhan.nif.alaudin@gmail.com>
2018-03-09 21:39:02 +0700

    Koneksi antara edit transaksi dan kas

--- proses/transaksi.php
+++ proses/transaksi.php
@@ -226,6 +226,9 @@ elseif(isset($_POST['updateTransaksi'])){
   $sisa_pelunasan = $total_tagihan - $dp;
   $hari = $_POST['jumhari'];
   $week = date("w",strtotime($check_in))+1;
+  $tanggal = date('Y-m-d H:i:s');
+  $keterangan = '6/'.$kd_transaksi;
+
   if($week>5){ //jika dimuai dari weekend
     $week_kind = explode("/",startinweekend($hari, $week, 0, 0));
     $jumlah_weekday = $week_kind[0]; $jumlah_weekend = $week_kind[1];
@@ -239,13 +242,39 @@ elseif(isset($_POST['updateTransaksi'])){
       $jumlah_weekend = $week_kind[1];
     }
   }
+
   $harga_asli = explode("/", $harga_sewa_asli);
   if($total_tagihan<(($harga_asli[0]*$jumlah_weekday)+($harga_asli[1]*$jumlah_weekend))){
     $diskon = getDisCount($harga_sewa, $harga_sewa_we, $harga_sewa_asli, $jumlah_weekday, $jumlah_weekend, $total_tagihan);
   }else{
     $diskon = 0;
   }
+
   $proses = new Transaksi($db);
+  $proses_kas = new Kas($db);
+
+
+  $show = $proses_kas->showKdKas($keterangan);
+  $data_mutasi = $show->fetch(PDO::FETCH_OBJ);
+  $kd_kas_lama = $data_mutasi->kd_kas;
+  $mutasi_dana = $data_mutasi->mutasi_dana;
+
+  //Mengembalikan Saldo kas yang diganti
+  $show_saldo_lama = $proses_kas->editSaldo($kd_kas_lama);
+  $data_saldo_lama = $show_saldo_lama->fetch(PDO::FETCH_OBJ);
+  $saldo_kas_lama = $data_saldo_lama->saldo - $mutasi_dana;
+
+  //Merubah Saldo kas pengganti
+  $show_saldo_baru = $proses_kas->editSaldo($kd_kas);
+  $data_saldo_baru = $show_saldo_baru->fetch(PDO::FETCH_OBJ);
+  $saldo_kas_baru = $data_saldo_baru->saldo + $dp;
+
+  $update_kas_lama = $proses_kas->updateKas($kd_kas_lama, $saldo_kas_lama, $tanggal);
+  $update_kas_baru = $proses_kas->updateKas($kd_kas, $saldo_kas_baru, $tanggal);
+
+  $delete_mutasi = $proses_kas->deleteMutasi($keterangan);
+  $add_mutasi = $proses_kas->addMutasiKas($kd_kas, $dp, 1, $tanggal, $keterangan);
+
   $update = $proses->updateUnit_kotor($kd_transaksi ,$kd_unit, $check_in, $check_out);
   $add = $proses->updateTransaksi($kd_transaksi, $kd_apt, $kd_unit, $tamu, $check_in, $check_out, $harga_sewa, $harga_sewa_we, $diskon, $ekstra_charge, $kd_booking, $kd_kas, $dp, $total_tagihan, $sisa_pelunasan, $hari, $jumlah_weekend, $jumlah_weekday);
   if($add == "Success"){
