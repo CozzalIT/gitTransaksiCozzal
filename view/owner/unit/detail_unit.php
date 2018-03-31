ffe6093100ca667ade3c3efb86ad31569633e4c0
yusufsopyan <yusuf.sopyan@gmail.com>
2018-03-31 08:19:12 +0700

    header dan footer owner

--- view/owner/unit/detail_unit.php
+++ view/owner/unit/detail_unit.php
@@ -256,9 +256,9 @@
 <!--end-main-container-part-->
 
 <!--Footer-part-->
-<div class="row-fluid">
-  <div id="footer" class="span12"> 2013 &copy; Matrix Admin. Brought to you by <a href="http://themedesigner.in">Themedesigner.in</a> </div>
-</div>
+<?php
+  include "../template/footer.php";
+?>
 <!--End-Footer-part-->
 <script src="../../../asset/js/slidegambar.js"></script>
 <!--
