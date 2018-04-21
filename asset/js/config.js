35105e90e57787b594d4d55b6c897b9317aaf7c6
abulaiz <ronabulaiz@gmail.com>
2018-04-21 14:16:19 +0700

    Pemutakhiran - Cek Mod Harga Saat Transaksi (Sementara masih off)

--- asset/js/config.js
+++ asset/js/config.js
@@ -1,3 +1,5 @@
+var harga_mod = [];
+
 //Menampilkan data Unit berdasarkan apartemen
 $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di load)
 	// Kita sembunyikan dulu untuk loadingnya
@@ -16,6 +18,14 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 			},
 			success: function(response){ // Ketika proses pengiriman berhasil
 					$("#unit").html(response.data_unit);
+					mod = response.mod_harga.split("  ");
+					for(i=1;i<mod.length;i++){
+						var s = mod[i].split(" ");
+						harga_mod[i-1] = {kd_unit : s[0], 
+							start_date: s[1], 
+							end_date: s[2], 
+							harga_sewa: s[3]};
+					}
 			},
 			error: function (xhr, ajaxOptions, thrownError) { // Ketika ada error
 				alert(thrownError); // Munculkan alert error
